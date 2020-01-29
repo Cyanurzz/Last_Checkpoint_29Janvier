4fc1aeed4bc3ca2336f39d52587b00e810307196
CyanurZz <jeanducellier.dev@gmail.com>
2020-01-29 10:26:20 +0100

    initialise project

---
+++ src/main/java/com/example/cyanurzz/CyanurzzApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyanurzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyanurzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyanurzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cyanurzz/CyanurzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyanurzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyanurzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
